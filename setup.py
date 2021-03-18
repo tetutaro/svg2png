9fad91e6798260e44eb9c5c35d4e73f259ce2448
Tetsutaro Maruyama <tetsutaro.maruyama@gmail.com>
2021-03-18 13:41:41 +0900

    read required packages from requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+from typing import List
 from setuptools import setup
 import svg2png
+
+
+def read_requirements() -> List[str]:
+    with open('requirements.txt', 'rt') as rf:
+        required = rf.read().strip().splitlines()
+    return required
+
 
 setup(
     name='svg2png',
@@ -11,10 +19,7 @@ setup(
     author='tetutaro',
     author_email='tetsutaro.maruyama@gmail.com',
     url='https://github.com/tetutaro/svg2png',
-    install_requires=[
-        'CairoSVG',
-        'numpy',
-    ],
+    install_requires=read_requirements(),
     entry_points={
         'console_scripts': [
             'svg2png = svg2png:main',
